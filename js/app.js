e9d55884713460e63a1af25850adafb1befb4255
JabedH <87475551+JabedH@users.noreply.github.com>
2022-03-09 18:54:40 +0600

    add

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 const myEmail = () => {
   const inputValue = document.getElementById("inputValue");
   const getValue = inputValue.value;
+  inputValue.value = "";
   const expression =
     /([a-zA-Z0-9]+)([\.{1}])?([a-zA-Z0-9]+)\@(?:gmail|GMAIL|mail)([\.])(?:com|COM)/g;
   console.log("click");
   if (expression.test(getValue)) {
     document.getElementById("output").innerHTML = " your email is valid";
+    document.getElementById("output").style.color = "green";
   } else {
-    document.getElementById("output").innerHTML = "your email is not valid";
+    document.getElementById("output").innerHTML = "please input valid email ";
+    document.getElementById("output").style.color = "red";
   }
 };
